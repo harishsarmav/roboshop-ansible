998180ffb5710fc395a84e5d82385f275dee4c86
Cloud User <centos@ip-172-31-8-24.ec2.internal>
2023-02-24 11:18:34 +0000

    Updated full setup

--- full-setup.sh
+++ full-setup.sh
@@ -1,4 +1,4 @@
-for component in frontend cart catalogue user shipping payment redis mongodb mysql rabbitmq ; do
+for component in frontend catalogue cart user shipping payment redis mongodb mysql rabbitmq ; do
 
   ansible-playbook -i ${component}-dev.harishsarma.online, roboshop.yml -e ansible_user=centos -e ansible_password=DevOps321 -e ROLE_NAME=${component} -e env=dev
 
